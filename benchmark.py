aaee2c8458099cf485e8ca39694ce53424ee6d3f
Nick Tidwell <nicktidwell52@gmail.com>
2024-03-26 22:47:45 -0400

    ad

--- benchmark.py
+++ benchmark.py
@@ -41,88 +41,88 @@ def load_args():
 #########################################
 
 
-
-def load_shikra_model(args):
-    model_name_or_path = args.model_path
-
-    model_args = Config(dict(
-        type='shikra',
-        version='v1',
-
-        # checkpoint config
-        cache_dir=None,
-        model_name_or_path=model_name_or_path,
-        vision_tower=r'openai/clip-vit-large-patch14',
-        pretrain_mm_mlp_adapter=None,
-
-        # model config
-        mm_vision_select_layer=-2,
-        model_max_length=2048,
-
-        # finetune config
-        freeze_backbone=False,
-        tune_mm_mlp_adapter=False,
-        freeze_mm_mlp_adapter=False,
-
-        # data process config
-        is_multimodal=True,
-        sep_image_conv_front=False,
-        image_token_len=256,
-        mm_use_im_start_end=True,
-
-        target_processor=dict(
-            boxes=dict(type='PlainBoxFormatter'),
-        ),
-
-        process_func_args=dict(
-            conv=dict(type='ShikraConvProcess'),
-            target=dict(type='BoxFormatProcess'),
-            text=dict(type='ShikraTextProcess'),
-            image=dict(type='ShikraImageProcessor'),
-        ),
-
-        conv_args=dict(
-            conv_template='vicuna_v1.1',
-            transforms=dict(type='Expand2square'),
-            tokenize_kwargs=dict(truncation_size=None),
-        ),
-
-        gen_kwargs_set_pad_token_id=True,
-        gen_kwargs_set_bos_token_id=True,
-        gen_kwargs_set_eos_token_id=True,
-    ))
-    training_args = Config(dict(
-        bf16=False,
-        fp16=True,
-        device='cuda',
-        fsdp=None,
-    ))
-
-    if args.load_in_8bit:
-        quantization_kwargs = dict(
-            quantization_config=BitsAndBytesConfig(
-                load_in_8bit=True,
-            )
-        )
-    else:
-        quantization_kwargs = dict()
-
-    model, preprocessor = load_pretrained_shikra(model_args, training_args, **quantization_kwargs)
-    if not getattr(model, 'is_quantized', False):
-        model.to(dtype=torch.float16, device=torch.device('cuda'))
-    if not getattr(model.model.vision_tower[0], 'is_quantized', False):
-        model.model.vision_tower[0].to(dtype=torch.float16, device=torch.device('cuda'))
-    print(
-        f"LLM device: {model.device}, is_quantized: {getattr(model, 'is_quantized', False)}, is_loaded_in_4bit: {getattr(model, 'is_loaded_in_4bit', False)}, is_loaded_in_8bit: {getattr(model, 'is_loaded_in_8bit', False)}")
-    print(
-        f"vision device: {model.model.vision_tower[0].device}, is_quantized: {getattr(model.model.vision_tower[0], 'is_quantized', False)}, is_loaded_in_4bit: {getattr(model, 'is_loaded_in_4bit', False)}, is_loaded_in_8bit: {getattr(model, 'is_loaded_in_8bit', False)}")
-
-    preprocessor['target'] = {'boxes': PlainBoxFormatter()}
-    tokenizer = preprocessor['text']
-    return preprocessor, tokenizer, model
-
 args = load_args()
-preprocessor, tokenizer, model = load_shikra_model(args)
+
+# def load_shikra_model(args):
+model_name_or_path = args.model_path
+
+model_args = Config(dict(
+    type='shikra',
+    version='v1',
+
+    # checkpoint config
+    cache_dir=None,
+    model_name_or_path=model_name_or_path,
+    vision_tower=r'openai/clip-vit-large-patch14',
+    pretrain_mm_mlp_adapter=None,
+
+    # model config
+    mm_vision_select_layer=-2,
+    model_max_length=2048,
+
+    # finetune config
+    freeze_backbone=False,
+    tune_mm_mlp_adapter=False,
+    freeze_mm_mlp_adapter=False,
+
+    # data process config
+    is_multimodal=True,
+    sep_image_conv_front=False,
+    image_token_len=256,
+    mm_use_im_start_end=True,
+
+    target_processor=dict(
+        boxes=dict(type='PlainBoxFormatter'),
+    ),
+
+    process_func_args=dict(
+        conv=dict(type='ShikraConvProcess'),
+        target=dict(type='BoxFormatProcess'),
+        text=dict(type='ShikraTextProcess'),
+        image=dict(type='ShikraImageProcessor'),
+    ),
+
+    conv_args=dict(
+        conv_template='vicuna_v1.1',
+        transforms=dict(type='Expand2square'),
+        tokenize_kwargs=dict(truncation_size=None),
+    ),
+
+    gen_kwargs_set_pad_token_id=True,
+    gen_kwargs_set_bos_token_id=True,
+    gen_kwargs_set_eos_token_id=True,
+))
+training_args = Config(dict(
+    bf16=False,
+    fp16=True,
+    device='cuda',
+    fsdp=None,
+))
+
+if args.load_in_8bit:
+    quantization_kwargs = dict(
+        quantization_config=BitsAndBytesConfig(
+            load_in_8bit=True,
+        )
+    )
+else:
+    quantization_kwargs = dict()
+
+model, preprocessor = load_pretrained_shikra(model_args, training_args, **quantization_kwargs)
+if not getattr(model, 'is_quantized', False):
+    model.to(dtype=torch.float16, device=torch.device('cuda'))
+if not getattr(model.model.vision_tower[0], 'is_quantized', False):
+    model.model.vision_tower[0].to(dtype=torch.float16, device=torch.device('cuda'))
+print(
+    f"LLM device: {model.device}, is_quantized: {getattr(model, 'is_quantized', False)}, is_loaded_in_4bit: {getattr(model, 'is_loaded_in_4bit', False)}, is_loaded_in_8bit: {getattr(model, 'is_loaded_in_8bit', False)}")
+print(
+    f"vision device: {model.model.vision_tower[0].device}, is_quantized: {getattr(model.model.vision_tower[0], 'is_quantized', False)}, is_loaded_in_4bit: {getattr(model, 'is_loaded_in_4bit', False)}, is_loaded_in_8bit: {getattr(model, 'is_loaded_in_8bit', False)}")
+
+preprocessor['target'] = {'boxes': PlainBoxFormatter()}
+tokenizer = preprocessor['text']
+    # return preprocessor, tokenizer, model
+
+# preprocessor, tokenizer, model = load_shikra_model(args)
 #Converted Function from Web Demo:
 def process_request(image_path, user_input):
     pil_image = Image.open(image_path).convert("RGB")
@@ -267,8 +267,17 @@ def parse_response(response):
 def get_truth_label():
     pass
 def get_noval_obj():
-     pass
+    dataset = json.load(open("./data/lvis_v1_val.json", "r"))
+    directory_path = "/datasets/MSCOCO17/val2017"
+    for filename in os.listdir(directory_path):
+        if filename.lower().endswith(".png"):
+            full_path = os.path.join(directory_path, filename)
+            input_query = "In the image, I need the bounding box coordinates of every object."
+                        # response = shikra(image_path, "In the image, I need the bounding box coordinates of every object.")
 
+            response = process_request(model, full_path, input_query)
+            target = get_truth_label()
+            pred = parse_response(response)
 def test_one_pass():
     input_img_path = "./000000111179.jpg"
     input_query = "Given the following image. Output the bounding box coordinates of each object in the image."
